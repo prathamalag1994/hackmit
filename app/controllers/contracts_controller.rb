f7ac3f46d5344d82769f82fa98ff4eca20c8a2c8
levdubinets@live.com <levdubinets.git@mailnull.com>
2014-10-05 07:12:30 -0700

    ethereummmmz

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -3,9 +3,17 @@ def new
 	@projects = Project.all
 end
 def create
-	
-    @contract = Contract.new(contract_params)
-    @contract.save
+  back_amount = contract_params["back"]
+  campaign_id = contract_params["projid"]
+  data = `serpent encode_datalist "'1' '#{campaign_id}'"`
+  secret = "6ad0b58c60ea450544775b0f468097e140b709c041fe23f2a7ae2138dc691096"
+  contract_address = "ab9998252af519f64e25668c5145ab4647d30e74"
+  gas_price  = "10000000000000000"
+  gas_amount = "4000"
+  transaction_str = contract_address + " " + back_amount + " " + gas_price + " " + gas_amount + " " secret + " " + data
+  `/home/colin/Documents/cpp-etherem/build/eth/eth transact #{transaction_str}`
+  @contract = Contract.new(contract_params)
+  @contract.save
   if @contract.save
   	@contract.custid = current_user.id
   	@proj = Project.find(@contract.projid)
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,17 @@ class ProjectsController < ApplicationController
 def new
 end
 def create
-   @project = Project.new(project_params)
+  target_amount = contract_params["target"]
+  user_id = current_user.ethid
+  seconds = contract_params["exp"] - Time.now
+  data = `serpent encode_datalist "'0' '#{user_id}' '#{seconds}' 'target_amount'"`
+  secret = "6ad0b58c60ea450544775b0f468097e140b709c041fe23f2a7ae2138dc691096"
+  contract_address = "ab9998252af519f64e25668c5145ab4647d30e74"
+  gas_price  = "10000000000000000"
+  gas_amount = "4000"
+  transaction_str = contract_address + " " + target_amount + " " + gas_price + " " + gas_amount + " " secret + " " + data
+  `/home/colin/Documents/cpp-etherem/build/eth/eth transact #{transaction_str}`
+  @project = Project.new(project_params)
   @project.save
   if @project.save
   	@project.backed = 0
